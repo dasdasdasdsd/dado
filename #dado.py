adbe05f67e13ff355f73834656c6a8e9512c78cb
dasdasdasdsd <100883452+dasdasdasdsd@users.noreply.github.com>
2022-03-04 00:26:38 +0100

    Add files via upload

---
+++ #dado.py
@@ -0,0 +1,36 @@
+#dado
+import cv2
+from os import path
+
+def dado():
+    import random
+    a=1;b=7
+    return random.randint(a, b-1)
+
+im = Image.open(r"C:\Users\stefano\Desktop\1.jpg") 
+n="1"
+im.show() 
+print("asdfghjkl")
+while n !="0" :
+    if(n=="1"):
+        print(dado())
+    n=input()
+
+    
+   
+  
+
+  
+
+  
+
+
+    
+
+        
+
+
+
+
+
+
